d0653343631f88de563012f8e2a7f2e3a4eed01b
Enzo Firmino <enzo.firmino@etu.umontpellier.fr>
2019-02-05 17:59:09 +0100

    first commit

--- src/main/java/fr/umontpellier/iut/exercice2/PrimeFactors.java
+++ src/main/java/fr/umontpellier/iut/exercice2/PrimeFactors.java
@@ -1,9 +1,18 @@
 package fr.umontpellier.iut.exercice2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
+
     public List<Integer> computeFactors(int number) {
-        throw new RuntimeException("Not yet implemented");
-    }
+        List<Integer> listNbPremier = new ArrayList<>();
+        int[] tabNbPremier = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+        while (number != 1) {
+
+        }
+
+
+
+
 }
